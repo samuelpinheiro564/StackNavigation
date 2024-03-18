a9de5baf8d3a1c27fd6c82f4ebcfebc375afced0
Samuel Pinheiro <samuel.pinheiro2@aluno.senai.br>
2024-03-18 14:31:34 -0300

    terminei

---
+++ src/data/Profile.js
@@ -0,0 +1,9 @@
+export const user = {
+    name: "Samuel Pinheiro",
+    email: "samuelpinheiro@dev.com",
+    phone: "1999999999",
+    address: {
+      city: "Campinas",
+      state: "São Paulo",
+    },
+  };
