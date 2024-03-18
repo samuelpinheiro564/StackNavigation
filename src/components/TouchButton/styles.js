35af8d34e020708fd162a61b249c3de3d593e6c5
Samuel Pinheiro <samuel.pinheiro2@aluno.senai.br>
2024-03-18 14:35:23 -0300

    foi

---
+++ src/components/TouchButton/styles.js
@@ -0,0 +1,15 @@
+
+const styles = StyleSheet.create({
+    title: {
+      fontSize: 24,
+      fontWeight: "bold",
+    },
+    button: {
+      marginTop: 20,
+      padding: 10,
+      backgroundColor: "#C9D4FF",
+      borderRadius: 5,
+    },
+  });
+  
+  export default styles;
